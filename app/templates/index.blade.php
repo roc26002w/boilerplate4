be7572033e81853a732b81f17f721e1b3587000c
Jace Ju <jaceju@gmail.com>
2015-01-09 12:24:53 +0800

    Inject assets.

---
+++ app/templates/index.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Laravel PHP Framework</title>
+<!-- build:css css/vendor.css -->
+<!-- bower:css -->
+<!-- endbower -->
+<!-- endbuid -->
+
+<!-- build:css css/main.css -->
+<link rel="stylesheet" href="styles/main.css">
+<!-- endbuild -->
+</head>
+<body>
+<h1>Hello world!</h1>
+
+
+<!-- build:js js/vender.js -->
+<!-- bower:js -->
+<!-- endbower -->
+<!-- endbuid -->
+
+<!-- build:js js/index.js -->
+<script src="scripts/index.js"></script>
+<!-- endbuid -->
+</body>
+</html>
