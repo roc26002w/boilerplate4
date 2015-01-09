3cfd449b308a1c9888c13735dede22800a1de419
Jace Ju <jaceju@gmail.com>
2015-01-09 16:54:04 +0800

    Fix tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -28,14 +28,14 @@ gulp.task('wiredep', ['templates'], function() {
         }))
         .pipe(gulp.dest('assets/styles'));
 
-    var bladeDeps = gulp.src('app/views/**/*.blade.php')
+    var tplDeps = gulp.src('app/views/**/*.blade.php')
         .pipe(wiredep({
             ignorePath: '../../public/',
             exclude: ['modernizr']
         }))
         .pipe(gulp.dest('app/views'));
 
-    return merge(styleDeps, bladeDeps);
+    return merge(styleDeps, tplDeps);
 });
 
 // Styles
@@ -43,7 +43,6 @@ gulp.task('styles', function() {
     return gulp.src('assets/styles/*.sass')
         .pipe($.plumber())
         .pipe($.rubySass({
-            compass: true,
             style: 'expanded',
             precision: 10
         }))
@@ -129,9 +128,9 @@ gulp.task('livereload', function () {
 gulp.task('watch', ['prepare'], function() {
     gulp.start('serve');
     gulp.start('livereload');
-    gulp.watch('app/templates/**/*.php', ['wiredep']);
+    gulp.watch(['app/templates/**/*.php', 'bower.json'], ['wiredep']);
     gulp.watch('assets/styles/**/*.scss', ['styles']);
-    gulp.watch('assets/scripts/**/*.js', ['scripts', 'jest']);
+    gulp.watch('assets/scripts/**/*.js', ['scripts']);
     gulp.watch('assets/images/**/*', ['images']);
 });
 
