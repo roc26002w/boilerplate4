580e82af315468e72ba62b63721538cdc2eeae06
Jace Ju <jaceju@gmail.com>
2015-01-29 18:29:14 +0800

    Add PHPUnit support.

--- tasks/watch.js
+++ tasks/watch.js
@@ -8,6 +8,7 @@ gulp.task('watch', ['prepare'], function() {
     gulp.start('serve');
     gulp.start('livereload');
     gulp.watch(['app/templates/**/*.php', 'bower.json'], ['wiredep']);
+    gulp.watch(['app/models/**/*.php', 'app/controllers/*.php'], ['phpunit']);
     gulp.watch('assets/styles/**/*.scss', ['styles']);
     gulp.watch('assets/scripts/**/*.js', ['scripts']);
     gulp.watch('assets/images/**/*', ['images']);
